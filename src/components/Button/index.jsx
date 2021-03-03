27b9e9c1fa0e1bbc4f3e395181b816c23a188d7a
sebknight <sebastianknightmaddox@gmail.com>
2021-03-03 20:57:31 +1300

    linting config

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -6,4 +6,9 @@ const Button = (props) => {
   return <button onClick={onClick}>{label}</button>;
 };
 
+Button.propTypes = {
+  onClick: propTypes.func.isRequired,
+  label: propTypes.string.isRequired
+}
+
 export default Button;
